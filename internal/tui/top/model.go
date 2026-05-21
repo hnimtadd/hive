fbc01d27c178bcc83d84e5eb951b4fd91b5c5bfe
Craftznake <minhdat15012002@gmail.com>
2026-05-21 23:58:03 +0700

    feat: move the handlers into separate files for better readability

--- internal/tui/top/model.go
+++ internal/tui/top/model.go
@@ -142,6 +142,7 @@ func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tui.ChangeModeMsg:
 		cmd = append(cmd, m.handleChangeModeMsg(msg))
+
 	case tui.ChangeStatusMsg:
 		if m.status != tui.Status(msg) {
 			m.status = tui.Status(msg)
@@ -278,6 +279,7 @@ func (m *model) handleSessionUpdate(update *agentv1.HiveSessionResponse) []tea.C
 			}
 			return cmds
 		}
+
 		if info := notification.GetInfo(); info != "" {
 			if taskID != "" {
 				cmds = append(cmds, tui.MsgCmd(chat.StreamChunkMsg{
@@ -399,62 +401,4 @@ func (m *model) resetConversation() {
 	m.requestToTask = map[string]string{}
 	m.startedTasks = map[string]struct{}{}
 	m.startStreamListener()
-}
-
-func (m *model) handleOpenConversationMsg(msg tui.OpenConversationMsg) tea.Cmd {
-	convID, respCh, err := m.client.StartConversation(m.ctx, msg.ConversationID)
-	if err != nil {
-		return tui.MsgCmd(tui.ErrorMsg(err))
-	}
-	// if current conversationID is not empty, which mean, we need to emit the
-	// signal that we need to clear the old conversation.
-	m.resetConversation()
-	m.registerConversation(convID, respCh)
-	m.content.Update(tui.ClearChatMsg{})
-	// at this point, the activeconversation is already available inside model
-	// we could subsitue and emit the message to the content model with the
-	// new convID.
-	msg.New = false
-	msg.ConversationID = convID
-	return m.content.Update(msg)
-}
-
-func (m *model) handleResizeMsg(msg tea.WindowSizeMsg) tea.Cmd {
-	m.height, m.width = msg.Height, msg.Width
-	mainHeight := max(0, msg.Height-tui.FooterHeight-tui.HeaderHeight)
-	m.header.Update(tea.WindowSizeMsg{
-		Height: tui.HeaderHeight,
-		Width:  m.width,
-	})
-	m.footer.Update(tea.WindowSizeMsg{
-		Height: tui.FooterHeight,
-		Width:  m.width,
-	})
-
-	m.content.Update(tea.WindowSizeMsg{
-		Height: mainHeight,
-		Width:  m.width,
-	})
-
-	m.help.Update(tea.WindowSizeMsg{
-		Height: mainHeight,
-		Width:  m.width,
-	})
-	return nil
-}
-
-func (m *model) handleChangeModeMsg(msg tui.ChangeModeMsg) tea.Cmd {
-	if m.mode != tui.Mode(msg) {
-		m.mode = tui.Mode(msg)
-		switch m.mode {
-		case tui.ModeNormal:
-			m.content.Update(tea.BlurMsg{})
-		case tui.ModeInsert:
-			m.content.Update(tea.FocusMsg{})
-		}
-		m.footer.Update(msg)
-		// Forward the mode change to chat so inputbar can update
-		return m.content.Update(msg)
-	}
-	return nil
 }
